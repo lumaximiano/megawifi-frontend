3479d7043e508017d984ceb4f0730e34f0d0e911
Lucas Maximiano <lukas-maximiano@hotmail.com>
2025-12-07 21:34:25 -0300

    fix: Simplifica a estrutura do hotspot para corrigir o build

--- source/hotspot-login/mainHotspot.jsx
+++ source/hotspot-login/mainHotspot.jsx
@@ -1,7 +1,7 @@
 // lmx-megawifi/frontend/src/hotspot.jsx - v3.0 - USA O COMPONENTE MESTRE
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ClientPortal from './styles/modules/components/ClientPortal'; // Importa o novo componente mestre
+import ClientPortal from './ClientPortal'; // Importa o novo componente mestre
 
 // A página agora apenas renderiza o componente mestre.
 // Como não passamos a propriedade 'previewData', ele automaticamente
